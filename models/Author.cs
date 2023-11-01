d50f17693798706e63c948c495198a4c18b0a4d4
Matheus Jessé <matheus_jesse@hotmail.com>
2023-11-01 19:09:15 -0300

    feat: criando a models de Authors

---
+++ models/Author.cs
@@ -0,0 +1,11 @@
+namespace book_api.models
+{
+    using System.ComponentModel.DataAnnotations;
+    
+    public class Author
+    {
+        [Key]
+        public int AuthorId {get; set;}
+        public string? Name {get; set;}
+    }
+}
